2fa1523ae81f7c3c591a9c2f3abc19f0797fb341
YouhannaWahba <43569300+YouhannaWahba@users.noreply.github.com>
2024-06-11 20:20:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Conver HTML elements from static elements to dynamic elemets
+// Deal with APIs
+// document.getElementById("march").innerHTML = "Hello World From JS File";
+document.getElementById("march").style.textAlign = "center";
+
+document.getElementById("march").style.color = "red";
